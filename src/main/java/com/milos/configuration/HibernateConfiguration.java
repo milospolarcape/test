db75cd76f2a2355f5d86b858fd85475ae7d3cc2e
Milos Nikolik <milospolarcape@gmail.com>
2016-03-29 14:13:48 +0200

    dunno

--- src/main/java/com/milos/configuration/HibernateConfiguration.java
+++ src/main/java/com/milos/configuration/HibernateConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan({ "com.websystique.springmvc.configuration" })
+@ComponentScan({ "com.milos.configuration" })
 @PropertySource(value = { "classpath:application.properties" })
 public class HibernateConfiguration {
 
